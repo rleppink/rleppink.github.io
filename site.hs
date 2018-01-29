8d6b6c8c50810116bceaf6e4eb8e7186776be028
Ronald Leppink <ronaldleppink@gmail.com>
2018-01-29 13:05:48 +0100

    Abstract out defaultcompiler

--- site.hs
+++ site.hs
@@ -1,15 +1,17 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.List
-import           Data.Monoid     (mappend)
+import           Data.Monoid           ((<>))
 import           Hakyll
-import           System.FilePath
+
+-- Force forward slash separators, even on Windows
+import           System.FilePath.Posix
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "images/**" $ do
         route   idRoute
         compile copyFileCompiler
 
@@ -23,10 +25,7 @@ main = hakyll $ do
         route cleanRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= cleanIndexUrls
-            >>= cleanIndexHtmls
-            >>= relativizeUrls
+            >>= defaultCompiler postCtx
 
 
     create ["archive.html"] $ do
@@ -34,15 +33,13 @@ main = hakyll $ do
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
+                    listField "posts" postCtx (return posts) <>
+                    constField "title" "Archief"             <>
                     defaultContext
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= cleanIndexUrls
-                >>= cleanIndexHtmls
-                >>= relativizeUrls
+                >>= defaultCompiler archiveCtx
 
 
     match "index.html" $ do
@@ -50,32 +47,29 @@ main = hakyll $ do
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    listField "posts" postCtx (return posts) <>
+                    constField "title" "Home"                <>
                     defaultContext
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= cleanIndexUrls
-                >>= cleanIndexHtmls
-                >>= relativizeUrls
+                >>= defaultCompiler indexCtx
+
 
     match "templates/*" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx =
-    dateField "date" "%e %B, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%e %B, %Y" <> defaultContext
 
 
 cleanRoute :: Routes
-cleanRoute = customRoute createIndexRoute
-    where
-    createIndexRoute ident = takeDirectory p </> takeBaseName p </> "index.html"
-                            where p = toFilePath ident
+cleanRoute =
+  customRoute createIndexRoute
+  where createIndexRoute ident =
+          takeDirectory p </> takeBaseName p </> "index.html"
+            where p = toFilePath ident
 
 
 cleanIndexUrls :: Item String -> Compiler (Item String)
@@ -94,3 +88,11 @@ cleanIndex url
     | idx `isSuffixOf` url = take (length url - length idx) url
     | otherwise            = url
   where idx = "index.html"
+
+
+defaultCompiler :: Context String -> Item String -> Compiler (Item String)
+defaultCompiler x y =
+  loadAndApplyTemplate "templates/default.html" x y
+    >>= cleanIndexUrls
+    >>= cleanIndexHtmls
+    >>= relativizeUrls
